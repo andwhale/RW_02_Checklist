0f7aa9550b34a51b11d6c0c8ea58b29b10400a90
andwhale <andwhale@gmail.com>
2017-12-27 17:58:44 +0300

    Refactored. One table view, 5 rows.

---
+++ Checklist/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  Checklist
+//
+//  Created by Andrew Velikiy on 12/27/17.
+//  Copyright © 2017 Andrey Velikiy. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+}
